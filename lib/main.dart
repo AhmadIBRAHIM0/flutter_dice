9ae3085ac0f625f4de1f904749d38dca93db8f49
aibrahim <ahmadji.ibr11@gmail.com>
2023-03-19 03:44:54 +0100

    finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,74 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(const DiceApp());
+}
+
+class DiceApp extends StatefulWidget {
+  const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int dice1 = 1;
+  int dice2 = 1;
+
+  void changeDice() {
+    setState(() {
+      dice1 = Random().nextInt(6) + 1;
+      dice2 = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Center(
+            child: Text(
+              'Dice',
+              style: TextStyle(
+                fontSize: 60,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset(
+                    'assets/images/Dice$dice1.png',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset(
+                    'assets/images/Dice$dice2.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
